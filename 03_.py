71133ba337cbb48487dcb8aa913cad4cd5e9dbb5
Nayeong99 <kimnvn99@gmail.com>
2021-08-04 22:09:17 -0400

    coin

--- 03_.py
+++ 03_.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import *
 from PyQt5 import uic
 import pykorbit
 
-form_class = uic.loadUiType("window.ui")[0]
+form_class = uic.loadUiType("코빗 시세 조회기.ui")[0]
 
 
 class MyWindow(QMainWindow, form_class):
@@ -14,7 +14,15 @@ class MyWindow(QMainWindow, form_class):
 
     def inquiry(self):
         price = pykorbit.get_current_price("BTC") # 조회 할 코인의 이름 넣기. 예: 
+        price2 = pykorbit.get_current_price("ETH")
+        price3 = pykorbit.get_current_price("XRP")
+        price4 = pykorbit.get_current_price("ADA")
+        
+        
         self.lineEdit.setText(str(price))
+        self.lineEdit_2.setText(str(price2))
+        self.lineEdit_3.setText(str(price3))
+        self.lineEdit_4.setText(str(price4))
         # 비트코인, 이더리움, 리플, 에이다
 
 
